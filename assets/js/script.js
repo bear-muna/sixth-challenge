bcb32648e88cc7c63aef6cb782ba715dfff6e9e9
bear-muna <bearmuna@gmail.com>
2023-04-08 01:19:44 -0700

    fix api

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var cityName = "London";
 var lat = undefined;
 var lon = undefined;
 var cityURL = undefined;
-var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=c17de62560563037ab3c738e326cb15f";
+var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid={api-key}";
 
 function getApi(requestUrl) {
   fetch(requestUrl)
@@ -15,7 +15,7 @@ function getApi(requestUrl) {
         console.log(data);
         lat = data[0].lat;
         lon = data[0].lon;
-        cityURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c17de62560563037ab3c738e326cb15f";
+        cityURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid={api-key}";
         console.log(lat);
         console.log(lon);
         console.log(cityURL);
