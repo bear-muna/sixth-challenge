19f4255bf8348fa4b37ed95845827f12e0a1ff05
bear-muna <bearmuna@gmail.com>
2023-04-08 01:13:29 -0700

    able to pull city api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// Variables
 var cityName = "London";
-
-
+var lat = undefined;
+var lon = undefined;
+var cityURL = undefined;
 var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=c17de62560563037ab3c738e326cb15f";
 
 function getApi(requestUrl) {
@@ -8,14 +10,33 @@ function getApi(requestUrl) {
     .then(function (response) {
       console.log(response);
       return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
+    })
+    .then(function (data) {
+        console.log(data);
+        lat = data[0].lat;
+        lon = data[0].lon;
+        cityURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=c17de62560563037ab3c738e326cb15f";
+        console.log(lat);
+        console.log(lon);
+        console.log(cityURL);
+        getApiCity(cityURL);
+    })};
 
-    var msg = "Your lattitude is: " + data[0].lat + 
-                "\nYour longitude is: " + data[0].lon;
 
-    console.log(msg);
-})};
+
+
+
+function getApiCity(cityURL) {
+    
+    fetch(cityURL)
+        .then(function(response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+};
+
 
 getApi(url);
